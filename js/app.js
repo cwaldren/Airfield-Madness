2ba8fa6d981583c804ed8042b9fd95f60ee07a47
cwaldren <casey@fzy.me>
2015-01-04 23:30:50 -0500

    plane flies

--- js/app.js
+++ js/app.js
@@ -32,8 +32,16 @@ var grass = new Image();
 grass.src = "http://localhost/Airfield-Madness/img/grass.png";
 
 var gameTime = 0;
-
+var mouse = { x:0, y:0, age:Date.now()};
+            
 var prevTime;
+
+var planeObj = {
+    pos: [400, 100],
+    rot: Math.PI,
+    holding:false,
+    alt: 1.0
+}
 function main() {
     var now = Date.now();
     var deltaTime = (now - prevTime) / 1000.0;
@@ -55,14 +63,95 @@ function render() {
     var pattern = ctx.createPattern(grass, 'repeat');
     ctx.fillStyle = pattern;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(runway, 400, 100);
+
+   
+    ctx.drawImage(runway, 400, 200);
+
+    ctx.save();
+    ctx.translate(planeObj.pos[0], planeObj.pos[1]);
+    ctx.rotate(planeObj.rot);
+    ctx.drawImage(plane, -plane.width/2, -plane.height/2, plane.width/planeObj.alt, plane.height/planeObj.alt);
+    ctx.restore();
 
 }
 
 function update(dt) {
     gameTime += dt;
+    updatePlane(dt)
     //update entities
     //check collisions etc.
 }
 
+function updatePlane(dt) {
+    planeObj.pos[0] += Math.cos(planeObj.rot-Math.PI/2);
+    planeObj.pos[1] += Math.sin(planeObj.rot-Math.PI/2);
+
+    if (input.isDown('a')) {
+        planeObj.rot -= .01;
+
+    }
+    if (input.isDown('d')) {
+        planeObj.rot += .01;
+    }
+
+     if (input.isDown('w')) {
+        if (planeObj.alt < 1.3)
+        planeObj.alt += .001;
+    }
+     if (input.isDown('s')) {
+        if (planeObj.alt > .8)
+        planeObj.alt -= .001;
+    }
+}
+
+function updatePlaneOld(dt) {
+    var dx =  planeObj.pos[0]- mouse.x ;//mouse.x - planeObj.pos[0];
+    var dy = planeObj.pos[1]-mouse.y  ;//mouse.y - planeObj.pos[1];
+
+    var dist = Math.sqrt(dx*dx + dy*dy);
+
+    plane.holding = (Date.now() - mouse.age > 1000 && (dist <= 100)) || dist < 100   ; 
+
+
+    if (plane.holding) {
+       
+        //center is the mouse
+        var theta = .5 * Math.PI/180;
+        var ax = Math.cos(theta) * dx - Math.sin(theta) * dy + mouse.x;
+        var ay = Math.sin(theta) * dx + Math.cos(theta) * dy + mouse.y;
+        planeObj.rot = Math.atan2(dy, dx) + Math.PI;
+        planeObj.pos[0] = ax;
+        planeObj.pos[1] = ay;
+}else{
+
+   
+
+var slope = dy/dx;
+    var angle = Math.atan2(dy, dx) ;
+    var dif = planeObj.rot - (angle + Math.PI/2);
+   planeObj.rot = (angle-Math.PI/2);
+    planeObj.pos[0] -= Math.cos(angle );
+    planeObj.pos[1] -= Math.sin(angle );
+    }
+}
+
 init();
+
+
+window.addEventListener('mousemove', updateMouse, false);
+
+function updateMouse(e) {
+    var pos = getMousePos(canvas, e);
+   
+    mouse.age = Date.now();
+    mouse.x = pos.x;
+    mouse.y = pos.y;
+
+}
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
---
+++ js/input.js
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2012, James Long
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+(function() {
+    var pressedKeys = {};
+
+    function setKey(event, status) {
+        var code = event.keyCode;
+        var key;
+
+        switch(code) {
+        case 32:
+            key = 'SPACE'; break;
+        case 37:
+            key = 'LEFT'; break;
+        case 38:
+            key = 'UP'; break;
+        case 39:
+            key = 'RIGHT'; break;
+        case 40:
+            key = 'DOWN'; break;
+        default:
+            // Convert ASCII codes to letters
+            key = String.fromCharCode(code);
+        }
+
+        pressedKeys[key] = status;
+    }
+
+    document.addEventListener('keydown', function(e) {
+        setKey(e, true);
+    });
+
+    document.addEventListener('keyup', function(e) {
+        setKey(e, false);
+    });
+
+    window.addEventListener('blur', function() {
+        pressedKeys = {};
+    });
+
+    window.input = {
+        isDown: function(key) {
+            return pressedKeys[key.toUpperCase()];
+        }
+    };
+})();
